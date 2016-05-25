96567c6d49ade70fbdb10808402c52f147eb1322
matthova <matthova87@gmail.com>
2016-05-25 00:35:25 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+To run this server you will need to install the following on your raspberry pi:
+
+- pip  
+```
+sudo apt-get install python-pip
+```
+- Flask
+```
+sudo pip install flask
+```
+
+To start the server
+```
+python app.py
+```
+
+To stop the server press "ctrl + x"
---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
+
+@app.route('/', methods=['GET'])
+def root():
+    return app.send_static_file('index.html')
+
+app.run(debug=True, host='0.0.0.0')
